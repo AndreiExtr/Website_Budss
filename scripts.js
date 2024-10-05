98a54d4f6bf2bf01e6796a55489ccee372ea3d73
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-10-05 20:32:31 +0300

    add cookies

--- scripts.js
+++ scripts.js
@@ -14,6 +14,26 @@ closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+// Показывать уведомление через 3 секунды после загрузки страницы
+window.addEventListener('load', () => {
+  setTimeout(() => {
+      const cookiesSection = document.getElementById('cookies');
+      cookiesSection.style.display = 'flex';
+  }, 3000); // 3000 миллисекунд = 3 секунды
+});
+
+// Обработка кнопок
+document.querySelector('.btn-accept').addEventListener('click', () => {
+  // Логика для принятия куки
+  document.getElementById('cookies').style.display = 'none'; // Скрыть уведомление
+});
+
+document.querySelector('.btn-decline').addEventListener('click', () => {
+  // Логика для отклонения куки
+  document.getElementById('cookies').style.display = 'none'; // Скрыть уведомление
+});
+
+
 
 // Закрытие меню при изменении размера окна
 window.addEventListener('resize', () => {
