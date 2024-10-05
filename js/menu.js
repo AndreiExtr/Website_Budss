a6ade1a5f422fe399ab299c7b8b2718a45d87eaa
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-10-05 21:27:00 +0300

    edit overflow

--- js/menu.js
+++ js/menu.js
@@ -7,18 +7,16 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
 
-    // Убираем или добавляем прокрутку
-    if (menu.classList.contains('active')) {
       document.body.style.overflow = 'hidden'; // Отключаем прокрутку
-    } else {
-        document.body.style.overflow = 'auto'; // Включаем прокрутку
-    }
+
 });
 
 // Закрытие меню при клике на крестик
 closeMenu.addEventListener('click', () => {
     burger.classList.remove('active');
     menu.classList.remove('active');
+
+      document.body.style.overflow = 'auto'; // Включаем прокрутку
 });
 
 // Закрытие меню при изменении размера окна
